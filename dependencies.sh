d544d7531ec238d73d895ee58d45fcb1b07e4ddf
Archit Gosain <architgosain0601@gmail.com>
2023-01-21 15:37:51 +0530

    dependencies and .zshrc

---
+++ dependencies.sh
@@ -0,0 +1,16 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'export PATH="/opt/homebrew/bin:$PATH"' >> $HOME/.zshrc
+source ~/.zshrc
+brew install fzf
+brew install bat
+brew install cmake
+brew install neovim
+git clone https://github.com/aviii06/nvim-basic-ide.git ~/.config/nvim
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+cp .zshrc ~/.zshrc
+source ~/.zshrc
